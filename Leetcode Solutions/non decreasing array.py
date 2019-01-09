f7c0f00d65478518c1975055362e2c50d4b42415
thoratomkar <omkarthorat21@gmail.com>
2019-01-09 11:50:27 -0500

    Python update

---
+++ Leetcode Solutions/non decreasing array.py
@@ -0,0 +1,13 @@
+def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        one, two = nums[:], nums[:]
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                one[i] = nums[i + 1]
+                two[i + 1] = nums[i]
+                break
+        return one == sorted(one) or two == sorted(two)
