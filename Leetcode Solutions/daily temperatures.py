4e0527fb01d37cf65b746be149f2d349255509f1
thoratomkar <omkarthorat21@gmail.com>
2019-01-26 08:59:02 -0500

    Leetcode Daily Commit

--- Leetcode Solutions/daily temperatures.py
+++ Leetcode Solutions/daily temperatures.py
@@ -24,3 +24,18 @@ def dailyTemperatures(self, T):
             stack.append(i)
 
         return ans
+
+        #--------------------------------------
+        n = len(T)
+        res = [0] * n
+        
+        for i in range(n - 2, -1, -1):
+            k = i + 1
+            
+            while T[i] >= T[k] and res[k] > 0:
+                k += res[k]
+                    
+            if T[k] > T[i]:
+                res[i] = k - i
+                
+        return res
